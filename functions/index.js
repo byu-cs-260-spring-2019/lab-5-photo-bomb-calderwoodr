4e71aceca99ecb33a5594b02e8a38f939e46dd62
Randon Calderwood <randon.calderwood@gmail.com>
2019-06-17 22:17:29 -0600

    70 plus effort

--- functions/index.js
+++ functions/index.js
@@ -9,5 +9,35 @@ const firebaseApp = firebase.initializeApp(
 const app = express();
 
 var db = firebase.firestore();
-
+var colRef = db.collection("quote_list");
 exports.app = functions.https.onRequest(app);
+
+// app.post('/api/post', async (req, res) => {
+//     try {
+//         let querySnapshot = await colRef.get();
+//         let index = parseInt(querySnapshot.docs.length);
+//         let numRecords = parseInt(querySnapshot.docs[index-1].id);
+//         let q = {
+//             id: numRecords + 1,
+//             quote: req.body.quote,
+//             author: req.body.author
+//         }
+//         colRef.doc(q.id.toString()).set(q);
+//         res.send(q);
+//     } catch (error) {
+//         console.log(error);
+//         res.sendStatus(500);
+//     }
+
+
+
+// });
+
+// app.get('/api/quotes', async (req, res) => {
+//     try{
+//         let querySnapshot = await colRef.get();
+//         res.send(querySnapshot.docs.map(doc => doc.data()));
+//     }catch(err){
+//         res.sendStatus(500);
+//     }
+// });
--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -1,12 +1,27 @@
 <template>
 <div>
   <div v-if="user" class="header">
+  <div class="container">
+    
+    
     <div>
-      <h2>{{user.email}}</h2>
+      <h2>Welcome {{user.email}}!</h2>
     </div>
     <div class="button">
       <p><button @click="logout" class="pure-button pure-button-primary">Logout</button></p>
     </div>
+    <br>
+    <br>
+    
+    <div class="update">
+      <h2>Upload a favorite Quote!</h2>
+      <div class="form"> 
+        <input v-model="quote" placeholder="Quote">
+        <input v-model="author" placeholder="Author">
+      </div>
+      <!-- <button @click="postQuote">Post Quote</button> -->
+    </div>
+  </div>
   </div>
   <div v-else>
     <router-link to="/register" class="pure-button">Register</router-link> or
@@ -16,8 +31,16 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'mypage',
+  // data: function () {
+  //   return {
+  //   quote: "",
+  //   author: "",
+  // };
+  // },
   computed: {
     user() {
       return this.$store.state.user;
@@ -34,6 +57,18 @@ export default {
         console.log(error);
       }
     },
+    async postQuote() {
+        try {
+          let resp = await axios.post('/api/post', 
+          {
+            quote: this.quote,
+            author: this.author
+          });
+          console.log("after axios", resp);
+        } catch (error) {
+          console.log(error);
+        }
+    }
   }
 }
 </script>
@@ -43,6 +78,16 @@ export default {
   margin: 0px 20px;
 }
 
+.update {
+  margin: 2rem;
+  min-width: 100%;
+}
+
+.container {
+  display: flex;
+  flex-wrap: wrap;
+
+}
 .header {
   display: flex;
 }
